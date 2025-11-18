8af77c8d90e708885828b1f4505ae211a8248e80
Pradhu Ramachandran <vidhuramachandran1@gmail.com>
2025-11-18 18:33:11 +0530

    Step-1 Test-case-2 (Handle a single number string) test case fails
    Wrote the next test case in step 1 to handle a single number string. The test case fails

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -5,9 +5,16 @@ void main() {
   group("String calculator step 1 group", () {
     test("Returns 0 on empty string", () {
       StringCalculator solution = StringCalculator();
-      int sum = solution.add("");
+      int expected = solution.add("");
 
-      expect(sum, 0);
+      expect(expected, 0);
+    });
+
+    test("Returns 1 for a single number string", () {
+      StringCalculator solution = StringCalculator();
+      int expected = solution.add("1");
+
+      expect(expected, 1);
     });
   });
 }
