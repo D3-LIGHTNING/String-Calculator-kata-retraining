af877fb047d51c88366bca5dfbd87962e0bf2a76
Pradhu Ramachandran <vidhuramachandran1@gmail.com>
2025-11-18 17:52:30 +0530

    initial commit
    The initial commit of the project with no source code and no test cases.

---
+++ bin/string_calculator_kata_retraining.dart
@@ -0,0 +1,5 @@
+import 'package:string_calculator_kata_retraining/string_calculator_kata_retraining.dart' as string_calculator_kata_retraining;
+
+void main(List<String> arguments) {
+  print('Hello world: ${string_calculator_kata_retraining.calculate()}!');
+}
---
+++ test/string_calculator_kata_retraining_test.dart
@@ -0,0 +1,8 @@
+import 'package:string_calculator_kata_retraining/string_calculator_kata_retraining.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
