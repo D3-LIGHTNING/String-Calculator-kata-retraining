9b49a287fb92f5d037ad74aafb9662270868df2e
Pradhu Ramachandran <vidhuramachandran1@gmail.com>
2025-11-19 16:28:02 +0530

    Step-3 test case-2 (Expect \n as separator) test case passed
    The method now expects \n along with , as separators.

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,7 +3,7 @@ class StringCalculator {
     if (input.isEmpty) return 0;
 
     int sum = 0;
-    List<String> numbers = input.split(",");
+    List<String> numbers = input.split(RegExp('[, \n]'));
 
     for (String number in numbers) {
       sum += int.parse(number);
